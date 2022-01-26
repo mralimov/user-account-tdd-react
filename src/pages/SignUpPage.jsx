96e7d9d135cf15a82696b2665e6854b911011eed
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2022-01-26 13:16:28 -0500

    conditional rendering form

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -7,10 +7,13 @@ const SignUpPage = () => {
     email: '',
     password: '',
     passwordRepeat: '',
+    error: {},
   };
 
   const [buttonDisabled, setButtonDisabled] = useState(true);
   const [objData, setObjData] = useState(initialState);
+  const [apiProgress, setApiProgress] = useState(false);
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
 
   const inputOnChange = (event) => {
     const { id, value } = event.target;
@@ -30,7 +33,7 @@ const SignUpPage = () => {
     }
   }, [password, passwordRepeat]);
 
-  const signUpHandler = (event) => {
+  const signUpHandler = async (event) => {
     event.preventDefault();
     console.log(objData);
 
@@ -39,16 +42,20 @@ const SignUpPage = () => {
       email,
       password,
     };
-
+    setApiProgress(true);
     // axios.post('/api/1.0/users', body);
-    fetch('/api/1.0/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    });
-    setObjData({ ...initialState });
+
+    try {
+      await fetch('/api/1.0/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+      setSignUpSuccess(true);
+      setObjData({ ...initialState });
+    } catch (error) {}
   };
 
   const submitHandler = (e) => {
@@ -56,48 +63,89 @@ const SignUpPage = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={submitHandler}>
-        <h1 className='text-center'>Sign Up</h1>
-
-        <div className='mb-3'>
-          <label htmlFor='username'>Username</label>
-          <input id='username' value={username} onChange={inputOnChange} />
-        </div>
-
-        <div className='mb-3'>
-          <label htmlFor='email'>E-mail</label>
-          <input id='email' value={email} onChange={inputOnChange} />
-        </div>
-
-        <div className='mb-3'>
-          <label htmlFor='password'>Password</label>
-          <input
-            value={password}
-            id='password'
-            type='password'
-            onChange={inputOnChange}
-          />
-        </div>
-
-        <div className='mb-3'>
-          <label htmlFor='passwordRepeat'>Password Repeat</label>
-          <input
-            value={passwordRepeat}
-            id='passwordRepeat'
-            type='password'
-            onChange={inputOnChange}
-          />
-        </div>
-
-        <button
-          className='btn btn-primary'
-          disabled={buttonDisabled}
-          onClick={signUpHandler}
+    <div className='col-lg-6 offset-lg-3 col-md-8 offset-md-2'>
+      {!signUpSuccess && (
+        <form
+          onSubmit={submitHandler}
+          className='card mt-5'
+          data-testid='form-sign-up'
         >
-          Sign Up
-        </button>
-      </form>
+          <div className='card-header'>
+            <h1 className='text-center'>Sign Up</h1>
+          </div>
+          <div className='card-body'>
+            <div className='mb-3'>
+              <label htmlFor='username' className='form-label'>
+                Username
+              </label>
+              <input
+                id='username'
+                className='form-control'
+                value={username}
+                onChange={inputOnChange}
+              />
+            </div>
+
+            <div className='mb-3'>
+              <label htmlFor='email' className='form-label'>
+                E-mail
+              </label>
+              <input
+                id='email'
+                className='form-control'
+                value={email}
+                onChange={inputOnChange}
+              />
+            </div>
+
+            <div className='mb-3'>
+              <label htmlFor='password' className='form-label'>
+                Password
+              </label>
+              <input
+                className='form-control'
+                value={password}
+                id='password'
+                type='password'
+                onChange={inputOnChange}
+              />
+            </div>
+
+            <div className='mb-3'>
+              <label htmlFor='passwordRepeat' className='form-label'>
+                Password Repeat
+              </label>
+              <input
+                className='form-control'
+                value={passwordRepeat}
+                id='passwordRepeat'
+                type='password'
+                onChange={inputOnChange}
+              />
+            </div>
+            <div className='text-center'>
+              <button
+                className='btn btn-primary'
+                disabled={buttonDisabled || apiProgress}
+                onClick={signUpHandler}
+              >
+                {apiProgress && (
+                  <span
+                    className='spinner-border spinner-border-sm'
+                    role='status'
+                  ></span>
+                )}
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </form>
+      )}
+      {signUpSuccess && (
+        <div className='alert alert-success mt-3' role='alert'>
+          Please check your e-mail to activate your account
+        </div>
+      )}
     </div>
   );
 };
